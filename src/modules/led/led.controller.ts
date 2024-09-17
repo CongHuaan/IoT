728adabbfe4ec8e1ef8be6f8c7a87685c711eefe
CongHuaan <huannc.b21cn403@stu.ptit.edu.vn>
2024-09-17 23:47:45 +0700

    mô tả api

--- src/modules/led/led.controller.ts
+++ src/modules/led/led.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Param, ParseIntPipe, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { LedService } from './led.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @ApiTags('led')
 @Controller('led')
@@ -9,6 +9,11 @@ export class LedController {
   constructor(private readonly ledService: LedService) {}
 
   @Get('paginated')
+  @ApiOperation({ summary: 'Phân trang' })
+  @ApiQuery({ name: 'page', required: true, description: 'Page number' })
+  @ApiQuery({ name: 'limit', required: true, description: 'Number of items per page' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async getPaginated(
     @Query('page') page: number,
     @Query('limit') limit: number,
@@ -23,16 +28,26 @@ export class LedController {
   }
 
   @Get('status')
+  @ApiOperation({ summary: 'Lấy status của thiết bị' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
   listStatus() {
     return this.ledService.listStatus();
   }
 
   @Post('turn-on/:id')
+  @ApiOperation({ summary: 'Bật thiết bị' })
+  @ApiParam({ name: 'id', required: true, description: 'ID of the LED to turn on' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async turnOn(@Param('id', ParseIntPipe) id: number) {
     return await this.ledService.turnOn(id);
   }
 
   @Post('turn-off/:id')
+  @ApiOperation({ summary: 'Tắt thiết bị' })
+  @ApiParam({ name: 'id', required: true, description: 'ID of the LED to turn off' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async turnOff(@Param('id', ParseIntPipe) id: number) {
     return await this.ledService.turnOff(id);
   }
--- src/modules/sensor/sensor.controller.ts
+++ src/modules/sensor/sensor.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { SensorService } from './sensor.service';
 import { Response } from 'express';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 
 @ApiTags('sensor')
 @Controller('sensor')
@@ -9,6 +9,9 @@ export class SensorController {
   constructor(private readonly sensorService: SensorService) {}
 
   @Get('all')
+  @ApiOperation({ summary: 'Lấy tất cả thông tin của sensor' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
+  @ApiResponse({ status: 404, description: 'Not found' })
   async getAll(@Res() res: Response) {
     try {
       const result = await this.sensorService.getAllSensors();
@@ -19,6 +22,15 @@ export class SensorController {
   }
 
   @Get('paginated')
+  @ApiOperation({ summary: 'Phân trang bao gồm tìm kiếm và sắp xếp' })
+  @ApiQuery({ name: 'page', required: true, description: 'Trang' })
+  @ApiQuery({ name: 'limit', required: true, description: 'Số bản ghi trên một trang' })
+  @ApiQuery({ name: 'sortBy', required: false, description: 'Sắp xếp' })
+  @ApiQuery({ name: 'order', required: false, description: 'Tăng hoặc giảm (asc/desc)' })
+  @ApiQuery({ name: 'searchField', required: false, description: 'Tìm kiếm' })
+  @ApiQuery({ name: 'searchValue', required: false, description: 'Giá trị tìm kiếm' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async getPaginated(
     @Query('page') page: number,
     @Query('limit') limit: number,
@@ -37,6 +49,9 @@ export class SensorController {
   }
 
   @Get('latest')
+  @ApiOperation({ summary: 'Lấy thông tin mới nhất của sensor' })
+  @ApiResponse({ status: 200, description: 'Successful response' })
+  @ApiResponse({ status: 400, description: 'Failed to fetch latest sensor data' })
   async getLatestRecord(@Res() res: Response) {
     try {
       const latestRecord = await this.sensorService.getLatestRecord();
@@ -45,5 +60,4 @@ export class SensorController {
       res.status(400).send('Failed to fetch latest sensor data');
     }
   }
-
 }
